ae6e8f9c609dd1db0f3a93ad585dc797494c527f
Mark Cornick <mark@markcornick.com>
2016-02-09 16:51:26 -0500

    vault 0.5.0

--- vault.rb
+++ vault.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Vault < Formula
   homepage 'http://www.vaultproject.io'
-  version '0.5.0-rc2'
+  version '0.5.0'
 
-  url 'https://releases.hashicorp.com/vault/0.5.0-rc2/vault_0.5.0-rc2_darwin_amd64.zip'
-  sha256 '317e6dfdeb9f0ebac25d40d878f5105a98a8071cc9d84681b155b69795eb2e47'
+  url 'https://releases.hashicorp.com/vault/0.5.0/vault_0.5.0_darwin_amd64.zip'
+  sha256 '1ab8150c91e4b293741d2a6d32085949b92be4d75073392a7ed52d6b55e2b132'
 
   depends_on :arch => :x86_64
 
