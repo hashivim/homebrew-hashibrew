bf02b5460f47171fa409de7c9438c784e67410bf
Mark Cornick <mark@markcornick.com>
2016-02-15 11:56:42 -0500

    Update Vault sha256sum

--- vault.rb
+++ vault.rb
@@ -5,7 +5,7 @@ class Vault < Formula
   version '0.5.0'
 
   url 'https://releases.hashicorp.com/vault/0.5.0/vault_0.5.0_darwin_amd64.zip'
-  sha256 '1ab8150c91e4b293741d2a6d32085949b92be4d75073392a7ed52d6b55e2b132'
+  sha256 '8f5ca5927f876737566a23442f098afa1ed3dc9d5b238c3c8f7563e06ab6c64c'
 
   depends_on :arch => :x86_64
 
