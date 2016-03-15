0ed283ea299c5d7525cfee697e936873f11c9f37
Mark Cornick <mark@markcornick.com>
2016-03-15 09:56:18 -0400

    packer 0.10.0

--- packer.rb
+++ packer.rb
@@ -1,9 +1,9 @@
 class Packer < Formula
   homepage 'https://www.packer.io'
-  version '0.9.0'
+  version '0.10.0'
 
-  url 'https://releases.hashicorp.com/packer/0.9.0/packer_0.9.0_darwin_amd64.zip'
-  sha256 'bd5f8ce38ceb866c3f1db2eb3d51091184dd021dd785d05bb47177b223fea9df'
+  url 'https://releases.hashicorp.com/packer/0.10.0/packer_0.10.0_darwin_amd64.zip'
+  sha256 'cb1d9768306466d566abc79d83911983ace6a9a6c0a6657dc7eaab03cabd2e21'
 
   depends_on :arch => :x86_64
 
