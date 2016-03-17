c6ff355333a44ce414ffb69cf76eb8ddb1552408
Mark Cornick <mark@markcornick.com>
2016-03-16 21:53:27 -0400

    consul 0.6.4

--- consul.rb
+++ consul.rb
@@ -1,9 +1,9 @@
 class Consul < Formula
   homepage 'https://www.consul.io'
-  version '0.6.3'
+  version '0.6.4'
 
-  url 'https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_darwin_amd64.zip'
-  sha256 '6dff4ffc61d66aacd627a176737b8725624718a9e68cc81460a3df9b241c7932'
+  url 'https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_darwin_amd64.zip'
+  sha256 '75422bbd26107cfc5dfa7bbb65c1d8540a5193796b5c6b272d8d70b094b26488'
 
   depends_on :arch => :x86_64
 
