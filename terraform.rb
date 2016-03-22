d8ddfaad094252a76b041718f09b4588ef06923b
Mark Cornick <mark@markcornick.com>
2016-03-22 09:56:18 -0400

    terraform 0.6.14

--- terraform.rb
+++ terraform.rb
@@ -1,9 +1,9 @@
 class Terraform < Formula
   homepage 'https://www.terraform.io'
-  version '0.6.13'
+  version '0.6.14'
 
-  url 'https://releases.hashicorp.com/terraform/0.6.13/terraform_0.6.13_darwin_amd64.zip'
-  sha256 '5f285ea0bf7f6bd704ef262330f88dc195ffa6ed118490d54961958dfe2dab24'
+  url 'https://releases.hashicorp.com/terraform/0.6.14/terraform_0.6.14_darwin_amd64.zip'
+  sha256 '9334f55a549d5cb3c583430be15e73b407bd7e115dc53db290381a482da17788'
 
   depends_on :arch => :x86_64
 
