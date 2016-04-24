d71b9934fc238b6e38bb9d965e1e359083294045
Mark Cornick <mark@markcornick.com>
2016-04-24 00:07:14 -0400

    nomad 0.3.2

--- nomad.rb
+++ nomad.rb
@@ -1,9 +1,9 @@
 class Nomad < Formula
   homepage 'https://www.nomadproject.io'
-  version '0.3.1'
+  version '0.3.2'
 
-  url 'https://releases.hashicorp.com/nomad/0.3.1/nomad_0.3.1_darwin_amd64.zip'
-  sha256 'c300a3b5c195fbccf26dfcdb756b4c953ca0e5e566a41fce1d10ef1947ec4279'
+  url 'https://releases.hashicorp.com/nomad/0.3.2/nomad_0.3.2_darwin_amd64.zip'
+  sha256 '96808b9b8056490026327bdc6614e1813cee7d42978676e0024085709fd3fcac'
 
   depends_on :arch => :x86_64
 
