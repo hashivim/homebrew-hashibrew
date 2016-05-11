6f7a3c3ca6f299c441edcb9c2b245f8be8264354
Mark Cornick <mark@markcornick.com>
2016-05-11 12:13:45 -0400

    terraform 0.6.16

--- terraform.rb
+++ terraform.rb
@@ -1,10 +1,10 @@
 class Terraform < Formula
   desc 'A tool for building, changing, and combining infrastructure safely and efficiently'
   homepage 'https://www.terraform.io'
-  version '0.6.15'
+  version '0.6.16'
 
-  url 'https://releases.hashicorp.com/terraform/0.6.15/terraform_0.6.15_darwin_amd64.zip'
-  sha256 '9cb305ac00b85e2575da3c71504f3fdd3f7ef61f35457af999c7b88802143311'
+  url 'https://releases.hashicorp.com/terraform/0.6.16/terraform_0.6.16_darwin_amd64.zip'
+  sha256 '23feb79263126877e6128a03c600cd626f6691a118a474694c5ad45cc5da9366'
 
   depends_on :arch => :x86_64
 
