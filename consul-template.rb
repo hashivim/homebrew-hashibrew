cc56756d9c539497b2139148c03a9e3bcacc6c8a
Mark Cornick <mark@markcornick.com>
2016-06-09 09:38:02 -0400

    consul-template 0.15.0

--- consul-template.rb
+++ consul-template.rb
@@ -2,9 +2,9 @@ class ConsulTemplate < Formula
   desc 'Generic template rendering and notifications with Consul'
   homepage 'https://github.com/hashicorp/consul-template'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/consul-template/0.14.0/consul-template_0.14.0_darwin_amd64.zip'
-  sha256 '37fc8aca13020180b367bb1d277242adb11526394f4f0ce1c3d5f0ec8d2071d8'
-  version '0.14.0'
+  url 'https://releases.hashicorp.com/consul-template/0.15.0/consul-template_0.15.0_darwin_amd64.zip'
+  sha256 '1ecc48bc3c5df7537e4857693322b46dc5db5563b84e8225b24df439e287f9e3'
+  version '0.15.0'
   def install
     bin.install Dir['*']
   end
