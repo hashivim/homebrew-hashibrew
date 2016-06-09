639edc3333d8f13db3599412b66bb2be1db7bf8f
Mark Cornick <mark@markcornick.com>
2016-06-09 09:38:02 -0400

    consul-replicate 0.2.0

--- consul-replicate.rb
+++ consul-replicate.rb
@@ -3,7 +3,7 @@ class ConsulReplicate < Formula
   homepage 'https://github.com/hashicorp/consul-replicate'
   depends_on :arch => :x86_64
   url 'https://releases.hashicorp.com/consul-replicate/0.2.0/consul-replicate_0.2.0_darwin_amd64.zip'
-  sha256 '4d5c944e33630750b5d73ae7f9fc0510322dd60efa04aaefa939bc0d59e93885'
+  sha256 '48956988c2f3d963930f48f26fe16c3dee9eede8719de002940f18802195c190'
   version '0.2.0'
   def install
     bin.install Dir['*']
