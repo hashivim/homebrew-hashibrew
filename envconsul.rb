c334bfa7469ebe748528933314618684bd608557
Mark Cornick <mark@markcornick.com>
2016-06-10 14:47:08 -0400

    envconsul 0.6.1

--- envconsul.rb
+++ envconsul.rb
@@ -3,7 +3,7 @@ class Envconsul < Formula
   homepage 'https://github.com/hashicorp/envconsul'
   depends_on :arch => :x86_64
   url 'https://releases.hashicorp.com/envconsul/0.6.1/envconsul_0.6.1_darwin_amd64.zip'
-  sha256 '86eb25aef204bbf8c1b61737431c1f8df585c30becaceabcc3eda7cf283ed203'
+  sha256 'bf35ecec47a6034cf7507150ed854e2d9425e588807d783ebfb53f83a5e5142b'
   version '0.6.1'
   def install
     bin.install Dir['*']
