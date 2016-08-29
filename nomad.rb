94060aea680ec80d6358ba979724e2f8bf82ac67
Mark Cornick <mark@markcornick.com>
2016-08-29 11:39:43 -0400

    nomad 0.4.1

--- nomad.rb
+++ nomad.rb
@@ -2,9 +2,9 @@ class Nomad < Formula
   desc 'A Distributed, Highly Available, Datacenter-Aware Scheduler'
   homepage 'https://www.nomadproject.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/nomad/0.4.0/nomad_0.4.0_darwin_amd64.zip'
-  sha256 '2f6bff636ae48343017e0e5edd4371f7abe434d1f70d95d7e28f08490c978c40'
-  version '0.4.0'
+  url 'https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_darwin_amd64.zip'
+  sha256 '5f2d52c73e992313e803fb29b6957ad1b754ed6e68bed5fa9fbe9b8e10a67aeb'
+  version '0.4.1'
   def install
     bin.install Dir['*']
   end
