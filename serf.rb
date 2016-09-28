01f4a1ddd0a805c268bb3edfb9d42155cdc7c8f7
Mark Cornick <mark@markcornick.com>
2016-09-28 11:33:29 -0400

    serf 0.8.0

--- serf.rb
+++ serf.rb
@@ -2,9 +2,9 @@ class Serf < Formula
   desc 'Service orchestration and management tool'
   homepage 'https://www.serfdom.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/serf/0.7.0/serf_0.7.0_darwin_amd64.zip'
-  sha256 '30ab3f2fe65cbef0ce1cefa27c7aa27043fc3634c52a2f46d8921964b1ab2380'
-  version '0.7.0'
+  url 'https://releases.hashicorp.com/serf/0.8.0/serf_0.8.0_darwin_amd64.zip'
+  sha256 '61bd6b853b33cc93d1de5ff639555360f677fffd2e5b9534e37faa20fa88387a'
+  version '0.8.0'
   def install
     bin.install Dir['*']
   end
