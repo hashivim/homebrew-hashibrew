757e499cf3dd9fe19a066a0a07a4a76877f99923
Mark Cornick <mark@markcornick.com>
2016-09-28 11:33:29 -0400

    terraform 0.7.4

--- terraform.rb
+++ terraform.rb
@@ -2,9 +2,9 @@ class Terraform < Formula
   desc 'A tool for building, changing, and combining infrastructure safely and efficiently'
   homepage 'https://www.terraform.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/terraform/0.7.2/terraform_0.7.2_darwin_amd64.zip'
-  sha256 '2a441124efd097007414545714927a9239980a5b0707384b0ee07badbae781cf'
-  version '0.7.2'
+  url 'https://releases.hashicorp.com/terraform/0.7.4/terraform_0.7.4_darwin_amd64.zip'
+  sha256 '21c8ecc161628ecab88f45eba6b5ca1fbf3eb897e8bc951b0fbac4c0ad77fb04'
+  version '0.7.4'
   def install
     bin.install Dir['*']
   end
