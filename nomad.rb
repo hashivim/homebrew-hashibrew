da2bd86eae1e4ff34ac14c96a2e599373d68a78c
Mark Cornick <mark@markcornick.com>
2016-11-14 15:22:34 -0500

    nomad 0.4.1

--- nomad.rb
+++ nomad.rb
@@ -6,9 +6,9 @@ class Nomad < Formula
   sha256 '5f2d52c73e992313e803fb29b6957ad1b754ed6e68bed5fa9fbe9b8e10a67aeb'
   version '0.4.1'
   devel do
-    url 'https://releases.hashicorp.com/nomad/0.5.0-rc1/nomad_0.5.0-rc1_darwin_amd64.zip'
-    sha256 '4e9c53c3e8d8fd6b34177b62d7b8e882ba1b8ea0d80decc0030e90c09df04e91'
-    version '0.5.0-rc1'
+    url 'https://releases.hashicorp.com/nomad/0.5.0-rc2/nomad_0.5.0-rc2_darwin_amd64.zip'
+    sha256 'e458684a4ceb65aa94829801f211a260394e97e8cabf2d3f3d6d289a08a41599'
+    version '0.5.0-rc2'
   end
   def install
     bin.install Dir['*']
