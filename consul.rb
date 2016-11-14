0cf6d50dd49d584a377b78cd09716199b577adbc
Mark Cornick <mark@markcornick.com>
2016-11-14 15:22:31 -0500

    consul 0.7.1

--- consul.rb
+++ consul.rb
@@ -2,9 +2,9 @@ class Consul < Formula
   desc 'A tool for service discovery, monitoring and configuration'
   homepage 'https://www.consul.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_darwin_amd64.zip'
-  sha256 '74111674527c5be0db7a98600df8290395abdd94e2cd86bda7418d748413396d'
-  version '0.7.0'
+  url 'https://releases.hashicorp.com/consul/0.7.1/consul_0.7.1_darwin_amd64.zip'
+  sha256 '9b3a199779a0d9b92266fae2abd7ed91c18ba437eba46e76114cd1940b3b7741'
+  version '0.7.1'
   def install
     bin.install Dir['*']
   end
