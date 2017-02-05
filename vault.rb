1d2e4336d7a04c7a42d2aa016db3b96a7978623c
Mark Cornick <mark@markcornick.com>
2017-02-05 11:00:27 -0500

    vault 0.6.4

--- vault.rb
+++ vault.rb
@@ -2,9 +2,9 @@ class Vault < Formula
   desc 'A tool for managing secrets'
   homepage 'https://www.vaultproject.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/vault/0.6.2/vault_0.6.2_darwin_amd64.zip'
-  sha256 'b5e6fadfed6a5226d96714f5ddcf7e042f3424d7489875af125543c967a706b7'
-  version '0.6.2'
+  url 'https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_darwin_amd64.zip'
+  sha256 '83f680b5e2aaa4c1c2a1a5dbc7479e63186233b6cdda64e24b968f6f1856b676'
+  version '0.6.4'
   def install
     bin.install Dir['*']
   end
