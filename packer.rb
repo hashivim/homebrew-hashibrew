3a1e797c3bf289ed98bab268e1999554c0ac5465
Mark Cornick <mark@markcornick.com>
2017-02-05 11:00:22 -0500

    packer 0.12.2

--- packer.rb
+++ packer.rb
@@ -2,9 +2,9 @@ class Packer < Formula
   desc 'A tool for creating identical machine images for multiple platforms from a single source configuration'
   homepage 'https://www.packer.io'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/packer/0.12.0/packer_0.12.0_darwin_amd64.zip'
-  sha256 'e3f25ad619f35e10a4195c971d78f29abceb16877bbf2bd75182140373d02bd3'
-  version '0.12.0'
+  url 'https://releases.hashicorp.com/packer/0.12.2/packer_0.12.2_darwin_amd64.zip'
+  sha256 'eef803cd43adc3b67593fd277f94b14ef266fe7124eb247d620beb29ebb4ec85'
+  version '0.12.2'
   def install
     bin.install Dir['*']
   end
