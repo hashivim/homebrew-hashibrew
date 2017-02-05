a6e7607e577b166857e04890290d2ca3bdb30244
Mark Cornick <mark@markcornick.com>
2017-02-05 11:00:12 -0500

    envconsul 0.6.2

--- envconsul.rb
+++ envconsul.rb
@@ -2,9 +2,9 @@ class Envconsul < Formula
   desc 'Read and set environmental variables for processes from Consul'
   homepage 'https://github.com/hashicorp/envconsul'
   depends_on :arch => :x86_64
-  url 'https://releases.hashicorp.com/envconsul/0.6.1/envconsul_0.6.1_darwin_amd64.zip'
-  sha256 'bf35ecec47a6034cf7507150ed854e2d9425e588807d783ebfb53f83a5e5142b'
-  version '0.6.1'
+  url 'https://releases.hashicorp.com/envconsul/0.6.2/envconsul_0.6.2_darwin_amd64.zip'
+  sha256 '6714e49c1155d07bace219b1a0cc572777da7f86bc51977d6aeefc0e44f07568'
+  version '0.6.2'
   def install
     bin.install Dir['*']
   end
